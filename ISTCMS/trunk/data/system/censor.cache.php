caed693fe8af80f235abea3bcb4e4b304aa23560
Haojia Che <chehaojia@gmail.com>
2010-08-28 14:16:18 +0000

    删除掉所有代码，改用GBK格式

--- ISTCMS/trunk/data/system/censor.cache.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-if(!defined('IN_SUPESITE')) exit('Access Denied');
-
-$_SGLOBAL['censor']=Array ('filter' => Array (),'banned' => '','mod' => '')
-
-?>
--- ISTCMS/trunk/ucenter/data/logs/201008.php
+++
@@ -1,2 +0,0 @@
-<?PHP exit;?>	UCenterAdministrator	unknown	1282984513	login	succeed
-<?PHP exit;?>	UCenterAdministrator	unknown	1282986081	login	succeed
