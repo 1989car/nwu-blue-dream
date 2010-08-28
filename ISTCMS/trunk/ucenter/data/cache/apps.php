b481a00c2832b7f649b874efad39e5dcf6b09acd
Haojia Che <chehaojia@gmail.com>
2010-08-28 14:55:03 +0000

    GBK版，所有与上一个版本均相同，只转换了编码

---
+++ ISTCMS/trunk/ucenter/data/cache/apps.php
@@ -0,0 +1,29 @@
+<?php
+$_CACHE['apps'] = array (
+  1 => 
+  array (
+    'appid' => '1',
+    'type' => 'SUPESITE',
+    'name' => 'ÐÅÏ¢¿ÆÑ§¼¼ÊõÑ§Ôº¡¤Èí¼þÑ§ÔºÑ§Éú¹¤×÷',
+    'url' => 'http://localhost/istcms',
+    'authkey' => 'Oboey8m7BbGaP5Q7ibF07bI1Y4d9D2ue60v5ubEaEb43A6d4F1UdS7A1r3Y0paB7',
+    'ip' => '',
+    'viewprourl' => '',
+    'apifilename' => 'uc.php',
+    'charset' => 'gbk',
+    'dbcharset' => 'gbk',
+    'synlogin' => '1',
+    'recvnote' => '1',
+    'extra' => '',
+    'tagtemplates' => '<?xml version="1.0" encoding="ISO-8859-1"?>
+<root>
+	<item id="template"><![CDATA[<a href="{url}" target="_blank">{subject}</a>]]></item>
+	<item id="fields">
+		<item id="subject"><![CDATA[×ÊÑ¶±êÌâ]]></item>
+		<item id="url"><![CDATA[×ÊÑ¶µØÖ·]]></item>
+	</item>
+</root>',
+  ),
+);
+
+?>
---
+++ ISTCMS/trunk/ucenter/data/config.inc.php
@@ -0,0 +1,18 @@
+<?php 
+define('UC_DBHOST', 'localhost');
+define('UC_DBUSER', 'root');
+define('UC_DBPW', 'mysql');
+define('UC_DBNAME', 'istcms');
+define('UC_DBCHARSET', 'gbk');
+define('UC_DBTABLEPRE', 'uc_');
+define('UC_COOKIEPATH', '/');
+define('UC_COOKIEDOMAIN', '');
+define('UC_DBCONNECT', 0);
+define('UC_CHARSET', 'gbk');
+define('UC_FOUNDERPW', 'af2d5c368eb434137715bca5ccc2f4c2');
+define('UC_FOUNDERSALT', '214174');
+define('UC_KEY', '7cevcDei1w1Jbz1n450X8R5O9L8H6GcI183tbJdubF27fS4o1DaI0bcv4T6M3N7v');
+define('UC_SITEID', '7LeYcJev141Bbk124d0U8Z5r9S8m6pcg193BbMdobP2Uft4a1saw0Vcf4M6X3F7n');
+define('UC_MYKEY', '7SeJcseE151Bbt1W4q068d5b9H826McH1P3dbndkb12wfn4p1QaU0Lc6486B3L70');
+define('UC_DEBUG', false);
+define('UC_PPP', 20);
